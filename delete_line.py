1bbb0412c19c3eeb3fe0226eea9f9b0476fd65c5
LingXii <649740559@qq.com>
2018-05-20 16:48:10 +0800

    better delete_line

--- delete_line.py
+++ delete_line.py
@@ -5,7 +5,7 @@ import numpy
 
 q = queue.Queue() # 广搜队列
 
-def bfs(img,vis):
+def bfs(img,vis,line_width):
     x_size = img.shape[0]
     y_size = img.shape[1]
     while(not q.empty()):
@@ -20,28 +20,54 @@ def bfs(img,vis):
                 q.put(right[i])
                 vis[right[i][0]][right[i][1]] = 1
         if (p[1]%12==0 and up[0] >= 0 and up[1]<y_size and vis[up[0]][up[1]] == 0 and img[up[0],up[1]]<30):
-            q.put(up)
-            vis[up[0]][up[1]] = 1
+            up_st = up[0]+line_width
+            if((up_st<x_size and img[up_st,up[1]]) or up_st>=x_size):
+                q.put(up)
+                vis[up[0]][up[1]] = 1
         if (p[1]%12==0 and down[0] < x_size and down[1]<y_size and vis[down[0]][down[1]] == 0 and img[down[0],down[1]]<30):
-            q.put(down)
-            vis[down[0]][down[1]] = 1
+            down_st = down[0]-line_width
+            if ((down_st < x_size and img[down_st, up[1]]) or down_st >= x_size):
+                q.put(down)
+                vis[down[0]][down[1]] = 1
     return img
 
 def del_line(img):
+    global line_width;
     img = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 165, 15)  # 二值化
     img = cv2.medianBlur(img, 3)  # 去噪
-    kernel = numpy.ones((3, 3), numpy.uint8)
-    img = cv2.erode(img, kernel, iterations=1)
+    # kernel = numpy.ones((3, 3), numpy.uint8)
+    # img = cv2.erode(img, kernel, iterations=1)
     x_size = img.shape[0]
     y_size = img.shape[1]
     visit = [[0 for y in range(y_size)] for x in range(x_size)]
+    line_count = 0;
+    sum = 0;
     for i in range(img.shape[0]):
         if (visit[i][0]==0 and img[i, 0] < 30):
+            sum = sum+1
+            if(i>0 and img[i,0]!=img[i-1,0]): line_count = line_count+1
             visit[i][0]=1
-            q.put((i,0));
-    img = bfs(img,visit)
+            q.put((i,0))
+    line_width = sum//line_count+2
+    img = bfs(img,visit,line_width)
     img = cv2.medianBlur(img, 3)  # 去噪
     cv2.imwrite("temp.jpg",img)
+
+
+    # 生成两张图片
+    # black = img.copy()
+    # white = img.copy()
+    # for i in range(x_size):
+    #     for j in range(y_size):
+    #         if(img[i,j]==128):
+    #             black[i,j]=255
+    #             white[i,j]=128
+    #         else:
+    #             black[i,j]=0
+    #             white[i,j]=255
+    # cv2.imwrite("black.png", black)
+    # cv2.imwrite("white.png", white)
+    #
     return img #返回一张图片，干扰线将用灰色（128）标出
 
 img = cv2.imread("4.jpg",0)
